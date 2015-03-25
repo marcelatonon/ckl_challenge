84d84e2cb79ddc7a1654f1ab0a6c877e18b6fea6
marcelatonon <marcela-admin@MBP-da-Marcela.lan>
2015-03-25 00:59:54 -0300

    Initial project with Restkit installed.

---
+++ ckl_challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ckl_challenge
+//
+//  Created by Marcela Tonon on 25/03/15.
+//  Copyright (c) 2015 Marcela Tonon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ckl_challenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ckl_challenge
+//
+//  Created by Marcela Tonon on 25/03/15.
+//  Copyright (c) 2015 Marcela Tonon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
